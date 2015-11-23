d9c302bdf42e1aad340f81345f376ab4d9a2192d
Avi Haiat <thaiat@ipelia.fr>
2015-11-23 17:19:46 +0200

    fix(app): Fix port when running on heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,6 @@ app = Flask(__name__)
 def main():
     return "Welcome Heroku Flask!"
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
