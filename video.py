052fd8e863957404d63c47e07f4b55962ba8305f
Avi Haiat <thaiat@ipelia.fr>
2015-11-24 04:06:22 +0200

    feat(app): Remove file saving

--- video.py
+++ video.py
@@ -1,10 +1,8 @@
 import re
 import cStringIO
-import sys
 from PIL import Image
 import numpy as np
 import cv2
-import base64
 
 IMAGE_SIZE = 200.0
 IMAGE_PADDING = 10
@@ -26,9 +24,8 @@ def process(content):
     image_data = re.sub('^data:image/.+;base64,', '', content).decode('base64')
     image = Image.open(cStringIO.StringIO(image_data))
     image = cv2.cvtColor(np.array(image), 2)
-    cv2.imwrite('image.jpg', image)
-    # cv2.imwrite('images/' + uuid + '.jpg', image)
-    #image = cv2.imread('images/slack_for_ios_upload.png')
+    # cv2.imwrite('image.jpg', image)
+
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
     faces = cascade.detectMultiScale(image, 1.03, 20)
@@ -44,7 +41,6 @@ def process(content):
             continue
         ratio = IMAGE_SIZE/obj.shape[1]
         obj = cv2.resize(obj, (int(IMAGE_SIZE), int(obj.shape[0]*ratio)))
-        #cv2.imwrite('tmp/'+ 'processed' + str(i) +'.jpg', obj)
         # find the keypoints and descriptors for object
         kp_o, des_o = orb.detectAndCompute(obj, None)
         if len(kp_o) == 0:
@@ -52,16 +48,7 @@ def process(content):
 
         # match descriptors
         matches = bf.match(des_r, des_o)
-        # matches = sorted(matches, key=lambda x: x.distance)
-        # print(str(len(matches)))
-        # store all the good matches as per Lowe's ratio test.
-        # Draw first 10 matches.
-        #img3 = python_utils.drawMatches(reference, kp_r, obj, kp_o, good[:10])
-        #cv2.imwrite('tmp/'+ 'processed-matches' + str(i) +'.jpg', img3)
 
-        # draw object on street image, if threshold met
-        #print('image ' + str(i) + ' ' + str(len(good)))
-        # print(len(matches))
         if(len(matches) >= MATCH_THRESHOLD):
             good.append({'x': x*1, 'y': y*1, 'width': w*1, 'height': h*1})
 
