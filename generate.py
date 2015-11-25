1d0c26f5e658110749fdb50e8077de6abf5c999b
totojab <eliottjabes@gmail.com>
2015-11-25 17:37:52 +0200

    fix(app): Random background color, cropping final image, clean

--- generate.py
+++ generate.py
@@ -14,9 +14,9 @@ from random import randint
 # im3 = Image.blend(im,im2,0.5)
 # im3.save('./images/generated/aaaaa.jpg')
 
-for i in range(0, 30):
-    x_offset = randint(20, 30)
-    y_offset = randint(25, 35)
+for i in range(0, 300):
+    x_offset = randint(45, 55)
+    y_offset = randint(57, 67)
     alpha = randint(0, 6) - 3
 
     pt1x = randint(0, 6) - 3
@@ -26,22 +26,34 @@ for i in range(0, 30):
     pt2y = randint(0, 6) - 3
     pt3y = randint(0, 6) - 3
 
+    rand_r = randint(0, 255)
+    rand_g = randint(0, 255)
+    rand_b = randint(0, 255)
+
     background = cv2.imread("./images/grey.jpg")
+
+    for j in range(0, 374):
+        for k in range(0, 300):
+            background.itemset((j, k, 0), rand_r)
+            background.itemset((j, k, 1), rand_g)
+            background.itemset((j, k, 2), rand_b)
+
     foreground = cv2.imread("./images/fifa_resized.jpg")
     cv2.imwrite('./images/generated/ORIGIN.jpg', foreground)
 
-    rows, cols, ch = background.shape
-
-    # shifted = imutils.translate(image, -100 + 10*i, -100 + 10*i)
     background[y_offset:y_offset+foreground.shape[0],
                x_offset:x_offset+foreground.shape[1]] = foreground
 
     rotated = convenience.rotate(background, alpha)
 
     pts1 = np.float32([[50, 50], [200, 50], [50, 200]])
-    pts2 = np.float32([[50+pt1x, 50+pt1y], [200+pt2x, 50+pt2y], [50+pt3x, 200+pt3y]])
+    pts2 = np.float32(
+        [[50+pt1x, 50+pt1y], [200+pt2x, 50+pt2y], [50+pt3x, 200+pt3y]])
     M = cv2.getAffineTransform(pts1, pts2)
 
+    rows, cols, ch = background.shape
     affineTransformed = cv2.warpAffine(rotated, M, (cols, rows))
 
-    cv2.imwrite('./images/generated/' + str(i) + '.jpg', affineTransformed)
+    final = affineTransformed[44:330, 35:265]  # cropping the image keeping the right proportions
+
+    cv2.imwrite('./images/generated/' + str(i) + '.jpg', final)
