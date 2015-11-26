e45de3449d4b8319167915513359b925c106a81d
Avi Haiat <thaiat@ipelia.fr>
2015-11-26 14:29:18 +0200

    feat(app): Try small classifier

--- video3.py
+++ video3.py
@@ -7,7 +7,7 @@ from imutils.object_detection import non_max_suppression
 
 IMAGE_SIZE = 80.0
 IMAGE_PADDING = 5
-MATCH_THRESHOLD = 300
+MATCH_THRESHOLD = 1
 orb = cv2.ORB(1000, 1.2)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 #cascade = cv2.CascadeClassifier('classifier/fifa.xml')
